d2296e2a6450f3daba42464a1635f4a7660b4f9f
Fred Proctor <frederick.proctor@nist.gov>
2016-07-15 14:58:49 -0400

    added mutexed DB

--- src/sample_client.cpp
+++ src/sample_client.cpp
@@ -8,12 +8,19 @@
 #include <stddef.h>		/* NULL, sizeof */
 #include <stdlib.h>		/* malloc, free, atoi */
 #include <string.h>		/* strncpy, strlen */
+#include <ctype.h>		/* isspace */
 #include <ulapi.h>
 #include "sample_app.h"		/* sample_app_init,exit */
 
 typedef struct {
+  ulapi_mutex_struct *mutex;
+  int count;
+} client_db_struct;
+
+typedef struct {
   void *client_task;
   ulapi_integer client_id;
+  client_db_struct *client_db_ptr;
   ulapi_integer debug;
 } client_args;
 
@@ -21,14 +28,18 @@ void client_code(void *args)
 {
   void *client_task;
   ulapi_integer client_id;
+  client_db_struct *client_db_ptr;
   ulapi_integer debug;
   enum {BUFFERLEN = 256};
   char inbuf[BUFFERLEN];
   char outbuf[BUFFERLEN];
   ulapi_integer nchars;
+  int count;
+  int lastcount = 0;
 
   client_task = ((client_args *) args)->client_task;
   client_id = ((client_args *) args)->client_id;
+  client_db_ptr = ((client_args *) args)->client_db_ptr;
   debug = ((client_args *) args)->debug;
   free(args);
 
@@ -38,8 +49,17 @@ void client_code(void *args)
   */
 
   for (;;) {
-    ulapi_snprintf(outbuf, sizeof(outbuf), "update");
-    ulapi_socket_write(client_id, outbuf, strlen(outbuf));
+    ulapi_mutex_take(client_db_ptr->mutex);
+    count = client_db_ptr->count;
+    ulapi_mutex_give(client_db_ptr->mutex);
+
+    if (count != lastcount) {
+      ulapi_snprintf(outbuf, sizeof(outbuf), "write %d", count);
+      lastcount = count;
+    } else {
+      ulapi_snprintf(outbuf, sizeof(outbuf), "read");
+    }
+    ulapi_socket_write(client_id, outbuf, strlen(outbuf)+1);
 
     nchars = ulapi_socket_read(client_id, inbuf, sizeof(inbuf)-1);
 
@@ -52,13 +72,13 @@ void client_code(void *args)
       if (debug) printf("end of file\n");
       break;
     }
-
     inbuf[nchars] = 0;
-    if (debug) printf("read %d chars: ``%s''\n", nchars, inbuf);
+
 
     /*
       Parse and handle the message here as your application requires.
     */
+    printf("%s\n", inbuf);
 
     ulapi_wait(1000000000);
   }
@@ -76,7 +96,10 @@ int main(int argc, char *argv[])
   ulapi_integer client_id;
   ulapi_task_struct *client_task;
   client_args *client_args_ptr;
+  client_db_struct client_db;
   char buffer[BUFFERLEN];
+  char *ptr;
+  int count;
 
   ulapi_opterr = 0;
 
@@ -137,19 +160,43 @@ int main(int argc, char *argv[])
     printf("serving port %d\n", (int) port);
   }
 
+  client_db.mutex = ulapi_mutex_new(0);
+  client_db.count = 0;
+
   client_task = ulapi_task_new();
   client_args_ptr = reinterpret_cast<client_args *>(malloc(sizeof(client_args)));
   client_args_ptr->client_task = client_task;
   client_args_ptr->client_id = client_id;
+  client_args_ptr->client_db_ptr = &client_db;
   client_args_ptr->debug = debug;
   ulapi_task_start(client_task, client_code, client_args_ptr, ulapi_prio_lowest(), 0);
 
   /* enter application main loop */
   while (!feof(stdin)) {
+
     if (NULL == fgets(buffer, sizeof(buffer), stdin)) {
       break;
     }
-    if ('q' == buffer[0]) break;
+
+    ptr = buffer;
+    while (isspace(*ptr)) ptr++;
+
+    if ('q' == *ptr) break;
+
+    if (0 == *ptr) {
+      ulapi_mutex_take(client_db.mutex);
+      count = client_db.count;
+      ulapi_mutex_give(client_db.mutex);
+      printf("%d\n", count);
+      continue;
+    }
+
+    if (1 == sscanf(ptr, "%d", &count)) {
+      ulapi_mutex_take(client_db.mutex);
+      client_db.count = count;
+      ulapi_mutex_give(client_db.mutex);
+      continue;
+    }
   }
 
   ulapi_socket_close(client_id);
--- src/sample_server.cpp
+++ src/sample_server.cpp
@@ -9,26 +9,40 @@
 #include <stddef.h>		/* NULL, sizeof */
 #include <stdlib.h>		/* malloc, free, atoi */
 #include <string.h>		/* strncpy, strlen */
+#include <ctype.h>		/* isspace */
 #include <ulapi.h>		/* ulapi_XXX */
 #include "sample_app.h"		/* sample_app_init,exit */
+
+/*
+  A simple shared database to show how mutual exclusion is achieved.
+*/
+typedef struct {
+  ulapi_mutex_struct *mutex;
+  int count;
+} server_db_struct;
 
 typedef struct {
   void *client_handler_task;
   ulapi_integer client_id;
-  ulapi_integer debug;
+  server_db_struct *server_db_ptr;
+  int debug;
 } client_handler_args;
 
 void client_handler_code(void *args)
 {
   void *client_handler_task;
   ulapi_integer client_id;
+  server_db_struct *server_db_ptr;
   int debug;
   enum {BUFFERLEN = 256};
   char inbuf[BUFFERLEN];
+  char outbuf[BUFFERLEN];
   ulapi_integer nchars;
+  int newcount;
 
   client_handler_task = ((client_handler_args *) args)->client_handler_task;
   client_id = ((client_handler_args *) args)->client_id;
+  server_db_ptr = ((client_handler_args *) args)->server_db_ptr;
   debug = ((client_handler_args *) args)->debug;
   free(args);
 
@@ -50,13 +64,27 @@ void client_handler_code(void *args)
 
     inbuf[nchars] = 0;
     if (debug) {
-      printf("read %d chars: ``%s''\n", nchars, inbuf);
+      printf("%s\n", inbuf);
     }
 
     /*
       Parse and handle the message here as your application requires.
     */
-    ulapi_socket_write(client_id, "beep", 5);
+    if (!strncmp(inbuf, "write", strlen("write"))) {
+      sscanf(inbuf, "%*s %d", &newcount);
+      ulapi_mutex_take(server_db_ptr->mutex);
+      server_db_ptr->count = newcount;
+      ulapi_mutex_give(server_db_ptr->mutex);
+    } else if (!strcmp(inbuf, "read")) {
+      ulapi_mutex_take(server_db_ptr->mutex);
+      newcount = server_db_ptr->count;
+      ulapi_mutex_give(server_db_ptr->mutex);
+    } else {
+      fprintf(stderr, "unknown request ``%s''\n", inbuf);
+    }
+    ulapi_snprintf(outbuf, sizeof(outbuf), "%d", newcount);
+    outbuf[sizeof(outbuf)-1] = 0;
+    ulapi_socket_write(client_id, outbuf, strlen(outbuf)+1);
   }
 
   ulapi_socket_close(client_id);
@@ -65,14 +93,16 @@ void client_handler_code(void *args)
 typedef struct {
   void *server_listen_task;
   ulapi_integer server_id;
-  ulapi_integer debug;
+  server_db_struct *server_db_ptr;
+  int debug;
 } server_listen_args;
 
 void server_listen_code(void *args)
 {
   void *server_listen_task;
   ulapi_integer server_id;
-  ulapi_integer debug;
+  server_db_struct *server_db_ptr;
+  int debug;
   ulapi_integer client_id;
   enum {BUFFERLEN = 256};
   char connection_addr[BUFFERLEN];
@@ -82,6 +112,7 @@ void server_listen_code(void *args)
 
   server_listen_task = ((server_listen_args *) args)->server_listen_task;
   server_id = ((server_listen_args *) args)->server_id;
+  server_db_ptr = ((server_listen_args *) args)->server_db_ptr;
   debug = ((server_listen_args *) args)->debug;
   free(args);
 
@@ -106,6 +137,7 @@ void server_listen_code(void *args)
     client_handler_args_ptr = reinterpret_cast<client_handler_args *>(malloc(sizeof(client_handler_args)));
     client_handler_args_ptr->client_handler_task = client_handler_task;
     client_handler_args_ptr->client_id = client_id;
+    client_handler_args_ptr->server_db_ptr = server_db_ptr;
     client_handler_args_ptr->debug = debug;
     ulapi_task_start(client_handler_task, client_handler_code, client_handler_args_ptr, ulapi_prio_lowest(), 0);
   }
@@ -117,12 +149,15 @@ int main(int argc, char *argv[])
 {
   int option;
   ulapi_integer port = SAMPLE_APP_DEFAULT_PORT;
-  ulapi_integer debug = 0;
+  int debug = 0;
   ulapi_integer server_id;
   ulapi_task_struct *server_listen_task;
   server_listen_args *server_listen_args_ptr;
+  server_db_struct server_db;
   enum {BUFFERLEN = 256};
   char buffer[BUFFERLEN];
+  char *ptr;
+  int count;
 
   ulapi_opterr = 0;
 
@@ -176,19 +211,43 @@ int main(int argc, char *argv[])
   }
   if (debug) printf("serving port %d\n", (int) port);
 
+  server_db.mutex = ulapi_mutex_new(0);
+  server_db.count = 0;
+
   server_listen_task = ulapi_task_new();
   server_listen_args_ptr = reinterpret_cast<server_listen_args *>(malloc(sizeof(server_listen_args)));
   server_listen_args_ptr->server_listen_task = server_listen_task;
   server_listen_args_ptr->server_id = server_id;
+  server_listen_args_ptr->server_db_ptr = &server_db;
   server_listen_args_ptr->debug = debug;
   ulapi_task_start(server_listen_task, server_listen_code, server_listen_args_ptr, ulapi_prio_lowest(), 0);
 
   /* enter application main loop */
   while (!feof(stdin)) {
+
     if (NULL == fgets(buffer, sizeof(buffer), stdin)) {
       break;
     }
-    if ('q' == buffer[0]) break;
+
+    ptr = buffer;
+    while (isspace(*ptr)) ptr++;
+
+    if ('q' == *ptr) break;
+
+    if (0 == *ptr) {
+      ulapi_mutex_take(server_db.mutex);
+      count = server_db.count;
+      ulapi_mutex_give(server_db.mutex);
+      printf("%d\n", count);
+      continue;
+    }
+
+    if (1 == sscanf(ptr, "%d", &count)) {
+      ulapi_mutex_take(server_db.mutex);
+      server_db.count = count;
+      ulapi_mutex_give(server_db.mutex);
+      continue;
+    }
   }
 
   ulapi_socket_close(server_id);
